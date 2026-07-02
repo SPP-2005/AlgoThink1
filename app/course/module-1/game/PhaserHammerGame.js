b38ea8efed8416fcad17b920edc54930899d0716
SPP-2005 <satyaprakashpatnaik12@gmail.com>
2026-07-02 15:33:25 +0530

    feat: implement interactive hammer game scene with character animations and nail physics in Phaser

--- app/course/module-1/game/PhaserHammerGame.js
+++ app/course/module-1/game/PhaserHammerGame.js
@@ -119,7 +119,7 @@ export function createHammerGame(Phaser, container, callbacks) {
             // Nail (origin bottom center, resting high on top of wood)
             // Scale X=0.15 (thin), Y=0.4 (tall) -> Height = 31.2px
             this.nailStart = tableY - 16.5; // Inserted 1px into the 17.5px tall wood block
-            this.nail = this.add.sprite(W / 2, this.nailStart, 'nail').setOrigin(0.5, 1).setScale(0.15, 0.2);
+            this.nail = this.add.sprite(W / 2, this.nailStart, 'nail').setOrigin(0.5, 1).setScale(0.15, 0.1);
 
             // Hammer on table (to be picked up)
             this.tableHammer = this.add.sprite(W / 2 + 50, tableY - 5, 'hammer').setOrigin(0.5, 0.5).setAngle(90).setScale(0.18);
